a9a61ef7b3d34280942d5a129d5caa5b83e5677c
Liuguxia <1102356056@qq.com>
2018-09-21 00:15:10 +0800

    文件拷贝 已把此封装成一个方法

--- src/CopyFileMethod.java
+++ src/CopyFileMethod.java
@@ -1,24 +1,16 @@
 import java.io.*;
 
-public class CopyFile {
-    /*
-            拷贝文件
-          1.建立联系，File对象（源头），另一File对象（目的地,文件可以不存在）
-          2.选择流
-               文件输入流  Inputstream、FileInputStream
-               文件输出流  OutputStream、FileOutputStream
-          3.操作  先读取，后输出
-               btye[] flush=new byte[1024]
-               int len=0
-               while(-1!=(len=输入流.read(flush))){
-                    输出流.write(flush,0,len);
-               }
-               输出流.flush();
-          4.释放资源，关闭流
-     */
+public class CopyFileMethod {
     public static void main(String[] args) {
-        File src = new File("C:\\Users\\pc\\Desktop\\In\\演示文稿1.pptx");
-        File desc = new File("C:\\Users\\pc\\Desktop\\Out\\演.pptx");
+        String srcPath="C:\\Users\\pc\\Desktop\\In\\演示文稿1.pptx";
+        String descPath="C:\\Users\\pc\\Desktop\\Out\\演示文稿copy.pptx";
+        copyFile(srcPath,descPath);
+    }
+
+    //封装的方法
+    public static void copyFile(String srcPath,String descPath){
+        File src = new File(srcPath);
+        File desc = new File(descPath);
         InputStream in = null;
         OutputStream out = null;
         try {
@@ -51,5 +43,4 @@ public class CopyFile {
             }
         }
     }
-
 }
